8e5a784a44723f6e13ddd48cef08e9f6bcd1b061
Syeda Batool Ali <49957553+batool110@users.noreply.github.com>
2020-03-28 14:33:31 +0500

    bottom nevigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,95 +1,156 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../widgets/Categories_circle.dart';
 import '../widgets/newArrival.dart';
 import '../widgets/popular_items.dart';
+import '../screens/categories_overview_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    Home(),
+    CategoriesOverviewScreen(),
+    Text(
+      'Cart',
+      style: optionStyle,
+    ),
+    Text(
+      'Account',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('E-Commerce Store')),
       ),
-      body: SafeArea(
-        child: Column(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.home),
+            title: Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.list),
+            title: Text('Categories'),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            title: Text('Cart'),
+          ),
+          BottomNavigationBarItem(
+            icon: FaIcon(FontAwesomeIcons.userCircle),
+            title: Text('User'),
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.cyan[300],
+        unselectedIconTheme: IconThemeData(color: Colors.grey),
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: MediaQuery.of(context).size.height / 4,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                    'https://datsgreathome.files.wordpress.com/2019/11/women-fashion.jpg?w=770'),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.circular(8.0)),
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Text(
+              '',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 28),
+            ),
+          ),
+        ),
+        Flexible(
+          child: CategoriesCircle(),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            Container(
-              height: MediaQuery.of(context).size.height / 4,
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: NetworkImage(
-                        'https://datsgreathome.files.wordpress.com/2019/11/women-fashion.jpg?w=770'),
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.circular(8.0)),
-              child: Align(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  '',
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 28),
+            Text(
+              'New Arrivals',
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            GestureDetector(
+              child: Text(
+                'See All',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                  decoration: TextDecoration.underline,
+                  decorationThickness: 2.85,
                 ),
               ),
             ),
-            Flexible(
-              child: CategoriesCircle(),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Text(
-                  'New Arrivals',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                GestureDetector(
-                  child: Text(
-                    'See All',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red,
-                      decoration: TextDecoration.underline,
-                      decorationThickness: 2.85,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            NewArrival(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Text(
-                  'Popular Items',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  ),
-                ),
-                GestureDetector(
-                  child: Text(
-                    'See All',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.red,
-                      decoration: TextDecoration.underline,
-                      decorationThickness: 2.85,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            PopularItems(),
           ],
         ),
-      ),
+        NewArrival(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Text(
+              'Popular Items',
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            GestureDetector(
+              child: Text(
+                'See All',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                  decoration: TextDecoration.underline,
+                  decorationThickness: 2.85,
+                ),
+              ),
+            ),
+          ],
+        ),
+        PopularItems(),
+      ],
     );
   }
 }
